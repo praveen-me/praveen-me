05881932140f2c50b3af2e49f741fb88d41bb36c
praveen-me <praveenkrsaini0@gmail.com>
2019-05-18 11:50:49 +0530

    Completed hero section

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,21 +25,12 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <main className="wrapper main">{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,13 @@ import './../scss/app.scss'
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `portfolio`, `personal website`, `blog`]} />
-    <div>
+    <div className="main__content">
+      {/* Hero Section */}
+      <div className="main__sec">
+        <h3 className="main__author-subhead center">Hi there! I'm</h3>
+        <h1 className="main__author-head center">Praveen Kumar Saini</h1>
+        <p className="main__author-experct">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa quos culpa dicta veritatis et iure impedit explicabo commodi. Culpa perferendis mollitia rem error veniam. <Link to="#" className="main__content-link">read more...</Link></p>
+      </div>
     </div>
   </Layout>
 )
