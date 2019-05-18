fae021571e1a6081944667f3419fe3c600e5ad1f
praveen-me <praveenkrsaini0@gmail.com>
2019-05-18 16:01:57 +0530

    Add about page and completed photo and header section in that

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,51 +1,64 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "gatsby"
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql} from 'gatsby';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-// const data = useStaticQuery(
-//   graphql`
-//     query {
-//       site {
-//         siteMetadata {
-//           title
-//         }
-//       }
-//     }
-//   `
-// )
+const About = ( { data } ) => {
+  const about = data.allMarkdownRemark.edges.filter(edge => edge.node.frontmatter.title === 'About');
+  
+  const minutes = Math.ceil( about[0].node.wordCount.words / 200 ) 
 
-class About extends Component  {
+  return (
+    <Layout>
+    <SEO title="About" keywords={[`gatsby`, `application`, `react`, `portfolio`, `personal website`, `blog`]}/>
 
-  state = {
-    isOpen: false
-  }
-
-  handleClick = () => {
-    this.setState({
-      isOpen: ! this.state.isOpen
-    })
-  }
-
-  render() {
-    const {isOpen } = this.state;
-    
-    return <Layout>
-      <SEO title="Page two" />
-      {/* <p>{ data.site.siteMetadata.title }</p>  */}
-      <p>Welcome to my about page.</p>
-      
-      {
-        isOpen ? 'Open' : "Close"
-      }
-
-      <button onClick={ this.handleClick }>Toogle</button>
-
-      <Link to="/">Go back to the homepage</Link>
+      {/* About Page */}
+      <div className="about sml-wrapper">
+        <div className="about__sec">
+          <div className="about__head-sec">
+            <h2 className="about__head">
+              About Me
+            </h2>
+            <p className="about__time"> { minutes } { minutes > 1 ? 'minutes' : 'minute'  }  read </p>
+          </div>
+          <img src={ data.imageOne.childImageSharp.fluid.src } alt="" className="about__author-img"/>
+        </div>
+        <p className="about__content">
+        
+        </p>
+      </div>    
+      <Link to="/">Home</Link>
     </Layout>
-  }
+  )
 }
+
+export const query = graphql`
+  query {
+    imageOne: file(relativePath: {eq: "me.jpg"}) {
+      childImageSharp {
+        fluid {
+          src
+          originalImg
+          originalName
+        }
+      }
+    }
+    allMarkdownRemark {
+      edges {
+        node {
+          html
+          wordCount {
+            words
+          }
+          frontmatter{
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About;
