87c84001c1daa6a52bfb03e37278ac32f83c16a5
praveen-me <praveenkrsaini0@gmail.com>
2019-05-29 01:40:41 +0530

    Add metatags for seo purpose for blogs

---
+++ public/styles-3f30f35dafb1c85edee3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{122:function(n,o,w){},154:function(n,o,w){}}]);
+//# sourceMappingURL=styles-3f30f35dafb1c85edee3.js.map
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import {  Location } from '@reach/router';
+
 import LayoutWrapper from '../components/layoutWrapper';
+// import SEO from '../components/seo';
+import Metatags from '../components/MetaTags';
 
 export default ( { data } ) => {
   const post = data.markdownRemark;
+  console.log(post)
+  const url = data.site.siteMetadata.siteUrl
+
   return (
     <LayoutWrapper>
-      <div className="content post">
-      <h2 className="post__title center"> { post.frontmatter.title }</h2>
-      <p className="post__info">  </p>
-      <div dangerouslySetInnerHTML={ { __html: post.html } } />
-      </div>
-
+      {/* <SEO title={ post.frontmatter.title }> */}
+        <Location>
+          {
+            ( locationProps ) => (
+              <Metatags
+              url={ url }
+              title= { post.frontmatter.title }
+              pathname={ locationProps.location.pathname }
+              description={ post.excerpt }
+              />
+            )
+          }
+        </Location>
+        <div className="content post">
+        <h2 className="post__title center"> { post.frontmatter.title }</h2>
+        <p className="post__info">  </p>
+        <div dangerouslySetInnerHTML={ { __html: post.html } } />
+        </div>
+      {/* </SEO> */}
     </LayoutWrapper>
   )
 }
 
+
 export const query = graphql`
   query($slug: String!) {
     markdownRemark( fields: { slug: { eq: $slug } } ) {
-      html,
+      html
+      excerpt
       frontmatter {
         title
       }
+    }
+    site {
+      siteMetadata {
+          siteUrl
+        }
     }
   }
 `
