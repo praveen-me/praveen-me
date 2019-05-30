db9b0acdd9cc91249c94e2ef2267a15cdddab589
praveen-me <praveenkrsaini0@gmail.com>
2019-05-30 23:47:44 +0530

    Add time stamp to the blog and date.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,11 +5,14 @@ import {  Location } from '@reach/router';
 import LayoutWrapper from '../components/layoutWrapper';
 // import SEO from '../components/seo';
 import Metatags from '../components/MetaTags';
+import { minutes } from '../utils/helper-functions';
 
 export default ( { data } ) => {
   const post = data.markdownRemark;
   const url = data.site.siteMetadata.siteUrl
 
+  const time = minutes(post.wordCount.words);
+  
   return (
     <LayoutWrapper>
       {/* <SEO title={ post.frontmatter.title }> */}
@@ -27,7 +30,7 @@ export default ( { data } ) => {
       </Location>
       <div className="content post">
         <h2 className="post__title center"> { post.frontmatter.title }</h2>
-        <p className="post__info">  </p>
+        <p className="post__info"> <span className="post__date">{ new Date(post.frontmatter.date).toDateString() }</span> - <span className="post__time">{ time } { time > 1 ? 'minutes' : 'minute' } read</span> </p>
         <div dangerouslySetInnerHTML={ { __html: post.html } } />
       </div>
     </LayoutWrapper>
@@ -40,8 +43,12 @@ export const query = graphql`
     markdownRemark( fields: { slug: { eq: $slug } } ) {
       html
       excerpt
+      wordCount {
+        words
+      }
       frontmatter {
         title
+        date
       }
     }
     site {
