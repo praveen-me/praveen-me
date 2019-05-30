9e46cb637e9ca0a6d3fd328801b7c18f4bf9c5e7
praveen-me <praveenkrsaini0@gmail.com>
2019-05-30 22:30:25 +0530

    Make content responsive on big screens

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,30 +8,28 @@ import Metatags from '../components/MetaTags';
 
 export default ( { data } ) => {
   const post = data.markdownRemark;
-  console.log(post)
   const url = data.site.siteMetadata.siteUrl
 
   return (
     <LayoutWrapper>
       {/* <SEO title={ post.frontmatter.title }> */}
-        <Location>
-          {
-            ( locationProps ) => (
-              <Metatags
-              url={ url }
-              title= { post.frontmatter.title }
-              pathname={ locationProps.location.pathname }
-              description={ post.excerpt }
-              />
-            )
-          }
-        </Location>
-        <div className="content post">
+      <Location>
+      {
+        ( locationProps ) => (
+          <Metatags
+          url={ url }
+          title= { post.frontmatter.title }
+          pathname={ locationProps.location.pathname }
+          description={ post.excerpt }
+          />
+        )
+      }
+      </Location>
+      <div className="content post">
         <h2 className="post__title center"> { post.frontmatter.title }</h2>
         <p className="post__info">  </p>
         <div dangerouslySetInnerHTML={ { __html: post.html } } />
-        </div>
-      {/* </SEO> */}
+      </div>
     </LayoutWrapper>
   )
 }
