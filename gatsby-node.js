a5c01a1a5a58316603906f47907177b129d4e9b9
praveen-me <praveenkrsaini0@gmail.com>
2019-06-01 21:37:39 +0530

    Remove comments from gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
 const { createFilePath } = require('gatsby-source-filesystem');
 const path = require('path');
 
