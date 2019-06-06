5f119bc87d09b093bcc181eb9c9ff4d09dac0f05
praveen-me <praveenkrsaini0@gmail.com>
2019-06-06 23:56:12 +0530

    Add Changes to gatsby config

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer__text center">
-      Made with <span role="img">❤️</span> JavaScript and SASS with React and Gatsby <span role="img">💻</span>
+      Made with <span role="img" alt="image of heart">❤️</span> JavaScript and SASS with React and Gatsby <span role="img" alt="image of computer">💻</span>
       </p> 
     </footer>
   )
