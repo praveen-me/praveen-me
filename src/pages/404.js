cb12acb6a9cb4843b1d06df29c0c0ab72a887ed7
praveen-me <praveenkrsaini0@gmail.com>
2019-06-08 15:29:50 +0530

    Resolve issue with 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import SEO from "../components/seo";
+import LayoutWrapper from './../components/layoutWrapper';
 
 const NotFoundPage = () => (
-  <Layout>
+  <LayoutWrapper>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </LayoutWrapper>
 )
 
-export default NotFoundPage
+export default NotFoundPage;
