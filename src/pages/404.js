35a64fd112e745b9ffdc63cd39b8d34e3a9497ae
praveen-me <praveenkrsaini0@gmail.com>
2019-06-08 15:54:40 +0530

    Add 404 images for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,9 @@ import LayoutWrapper from './../components/layoutWrapper';
 
 const NotFoundPage = () => (
   <LayoutWrapper>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div className="not-found">
+    </div>
   </LayoutWrapper>
 )
 
