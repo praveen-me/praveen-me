297910e876f57dc7cb6d2cc8d9d9e47983903b54
praveen-me <praveenkrsaini0@gmail.com>
2019-06-25 00:53:33 +0530

    Fix some issue with grammar in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,11 +21,11 @@ const About = ( { data } ) => {
           <img src={ data.imageOne.childImageSharp.fluid.src } alt="Praveen Kumar Saini" className="about__author-img"/>
         </div>
         <p className="about__content content">
-          <p>I am a life long learner and currently working as a React Developer at <a href="https://rtcamp.com">rtCamp</a> . Most of the time I totally evolved in JavaScript stuff like React, Redux, Node, Express, MongoDB etc.</p> 
+          <p>I am a life long learner and currently working as a React Developer at <a href="https://rtcamp.com">rtCamp</a> . Most of the time I am totally involved in JavaScript stuff like React, Redux, Node, Express, MongoDB etc.</p> 
 
           <p>As we know the web is continuously changing so I always try to be updated and always be evolved around the new technologies and always have my keen eye on them for making something good out of them.</p> 
 
-          <p>I am a college dropout, Google Udacity Scholar and got a Front End Nanodegree. I also did a Full Stack Development Program from <a href="https://altcampus.io">AltCampus</a> and always greatful of their program that helps me to be quality developer.</p> 
+          <p>I am a college dropout, Google Udacity Scholar and got a Front End Nanodegree. I also did a Full Stack Development Program from <a href="https://altcampus.io">AltCampus</a> and always grateful of their program that helps me to be quality developer.</p> 
 
           <p>In this blog, I like to write about JavaScript stuff i.e what I explored and find interesting to write about.</p> 
           <p>In my free time, i.e when I am not coding I like to sleep <span aria-label="Grin Image" role="img" alt="image of grin">😬</span>, read philosophies, watch documentaries and Netflix.</p> 
