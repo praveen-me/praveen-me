6b1b84100b8a7fe26879904a16f12e1b22720c13
praveen-me <praveenkrsaini0@gmail.com>
2019-08-12 23:48:20 +0530

    Run prettier

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,12 @@
 import React from "react";
 import SEO from "../components/seo";
-import LayoutWrapper from './../components/layoutWrapper';
+import LayoutWrapper from "./../components/layoutWrapper";
 
 const NotFoundPage = () => (
   <LayoutWrapper>
     <SEO title="404" />
-    <div className="not-found">
-    </div>
+    <div className="not-found"></div>
   </LayoutWrapper>
-)
+);
 
 export default NotFoundPage;
