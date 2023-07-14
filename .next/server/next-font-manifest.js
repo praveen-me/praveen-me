183810cd6ffa0c4a3f13da92ffba47816ea491d0
praveen-me <praveenkrsaini0@gmail.com>
2023-07-14 11:54:49 +0530

    Fix: content layer

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/praveenkumarsaini/Documents/learn/praveen-me.github.io/app/layout\":[\"static/media/5863dc867561b465-s.p.ttf\",\"static/media/c78e5b1fb9b48c59-s.p.ttf\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
